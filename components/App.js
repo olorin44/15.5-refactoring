60bf885146a4967a7ed23bb6c2b4c467faf67c73
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2018-07-28 09:57:42 +0200

    Add promises

--- components/App.js
+++ components/App.js
@@ -10,72 +10,54 @@ App = React.createClass({
         };
     },
 
-    // handleSearch: function(searchingText) {
-    //     this.setState({
-    //       loading: true
-    //     });
-    //     this.getGif(searchingText, function(gif) {
-    //       this.setState({
-    //         loading: false,
-    //         gif: gif,
-    //         searchingText: searchingText
-    //       });
-    //     }.bind(this));
-    //   },
+    handleSearch: function(searchingText) {  
+            
+            this.setState({
+              loading: true  
+            });
+            
+            var self = this;
+            this.getGifPromise(searchingText)
 
-////////////////////testy/////////////////////
-handleSearch: function(searchingText) {
-        this.setState({
-          loading: true
-        });
-        getGifPromise();
-        .then(response => console.log('jest ok:  ' + response))
-        .catch(error => console.log('jest do dupy:  ', error));
-          this.setState({
-            loading: false,
-            gif: gif,
-            searchingText: searchingText
-          });
-      },
-////////////////////testy//////////////////////
-    getGifPromise: function() {
-        let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+                .then(function(gif) {  
+                
+                    self.setState({  
+                        loading: false,  
+                        gif: gif,  
+                        searchingText: searchingText  
+                      });
+            })
+
+},
+
+    getGifPromise: function(searchingText) {
         return new Promise (
             function(resolve, reject) {
+
+                let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
                 const request = new XMLHttpRequest();
+                request.open('GET', url);
                 request.onload = function() {
-                    if(this.status === 200) {
-                        resolve(this.response);
+
+                    if(request.status === 200) {
+                        let data = JSON.parse(request.responseText).data;
+                        let gif = {
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        resolve(gif);
                     } else {
                         reject(new Error(this.statusText));
                     }
                 };
+
                 request.onerror = function() {
                     reject(new Error(
                         `XMLHttpRequest Error: ${this.statusText}`));
                 };
-                request.open('GET', url);
                 request.send();
             });
     },
-////////////////////testy//////////////////
-
-    // getGif: function(searchingText, callback) {
-    //     var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open('GET', url);
-    //     xhr.onload = function() {
-    //         if (xhr.status === 200) {
-    //            var data = JSON.parse(xhr.responseText).data;
-    //             var gif = {
-    //                 url: data.fixed_width_downsampled_url,
-    //                 sourceUrl: data.url
-    //             };
-    //             callback(gif);
-    //         }
-    //     };
-    //     xhr.send();
-    // },
 
     render: function() {
 
